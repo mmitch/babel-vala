36fd2484a417246339c85ef5f3ab3775571a1261
Christian Garbs <mitch@cgarbs.de>
2017-03-28 23:04:28 +0200

    update comments, remove template comments

--- ob-vala.el
+++ ob-vala.el
@@ -26,82 +26,52 @@
 
 ;;; Commentary:
 
-;; This file is not intended to ever be loaded by org-babel, rather it
-;; is a template for use in adding new language support to Org-babel.
-;; Good first steps are to copy this file to a file named by the
-;; language you are adding, and then use `query-replace' to replace
-;; all strings of "template" in this file with the name of your new
-;; language.
-;;
-;; If you have questions as to any of the portions of the file defined
-;; below please look to existing language support for guidance.
-;;
-;; If you are planning on adding a language to org-babel we would ask
-;; that if possible you fill out the FSF copyright assignment form
-;; available at http://orgmode.org/request-assign-future.txt as this
-;; will make it possible to include your language support in the core
-;; of Org-mode, otherwise unassigned language support files can still
-;; be included in the contrib/ directory of the Org-mode repository.
+;; ob-vala.el provides Babel support for the Vala language
+;; (see http://live.gnome.org/Vala for details)
 
 ;;; Requirements:
 
-;; Use this section to list the requirements of this language.  Most
-;; languages will require that at least the language be installed on
-;; the user's system, and the Emacs major mode relevant to the
-;; language be installed as well.
+;; - Vala compiler binary (valac)
+;; - Vala development environment (Vala libraries etc.)
+;;
+;; vala-mode.el is nice to have for code formatting, but is not needed
+;; for ob-vala.el
 
 ;;; Code:
 (require 'ob)
 (require 'ob-ref)
 (require 'ob-comint)
 (require 'ob-eval)
-;; possibly require modes required for your language
 
-;; optionally define a file extension for this language
+;; file extension
 (add-to-list 'org-babel-tangle-lang-exts '("vala" . "vala"))
 
-;; optionally declare default header arguments for this language
+;; header arguments empty by default
 (defvar org-babel-default-header-args:vala '())
 
 (defvar org-babel-vala-compiler "valac"
   "Command used to compile a Vala source code file into an
 executable.")
 
-;; This function expands the body of a source code block by doing
-;; things like prepending argument definitions to the body, it should
-;; be called by the `org-babel-execute:vala' function below.
 (defun org-babel-expand-body:vala (body params &optional processed-params)
-  "Expand BODY according to PARAMS, return the expanded body."
-  body ;; TODO: do nothing for now, just return the original body
+  "Expand BODY: does nothing, returns original BODY while ignoring PARAMS."
+  body ;; TODO: expand PARAMS?
   )
-  ;; (require 'inf-vala)
-  ;; (let ((vars (nth 1 (or processed-params (org-babel-process-params params)))))
-  ;;   (concat
-  ;;    (mapconcat ;; define any variables
-  ;;     (lambda (pair)
-  ;;       (format "%s=%S"
-  ;;               (car pair) (org-babel-vala-var-to-vala (cdr pair))))
-  ;;     vars "\n") "\n" body "\n")))
 
 ;; This is the main function which is called to evaluate a code
 ;; block.
 ;;
-;; This function will evaluate the body of the source code and
-;; return the results as emacs-lisp depending on the value of the
-;; :results header argument
-;; - output means that the output to STDOUT will be captured and
-;;   returned
-;; - value means that the value of the last statement in the
-;;   source code block will be returned
+;; - run Vala compiler and create a binary in a temporary file
+;;   - compiler/linker flags can be set via :flags header argument
+;; - if compilation succeeded, run the binary
+;;   - commandline parameters to the binary can be set via :cmdline
+;;     header argument
+;;   - stdout will be parsed as RESULT (control via :result-params
+;;     header argument)
 ;;
-;; The most common first step in this function is the expansion of the
-;; PARAMS argument using `org-babel-process-params'.
+;; There is no session support because Vala is a compiled language.
 ;;
-;; Please feel free to not implement options which aren't appropriate
-;; for your language (e.g. not all languages support interactive
-;; "session" evaluation).  Also you are free to define any new header
-;; arguments which you feel may be useful -- all header arguments
-;; specified by the user will be available in the PARAMS variable.
+;; This function is heavily based on ob-C.el (licensed under GPL 3+).
 (defun org-babel-execute:vala (body params)
   "Execute a block of Vala code with org-babel.
 This function is called by `org-babel-execute-src-block'"
@@ -141,35 +111,7 @@ This function is called by `org-babel-execute-src-block'"
 	    (cdr (assoc :rowname-names params)) (cdr (assoc :rownames params)))
 	   ))
       )))
-;; (let* ((processed-params (org-babel-process-params params))
-;;          ;; set the session if the session variable is non-nil
-;;          (session (org-babel-vala-initiate-session (first processed-params)))
-;;          ;; variables assigned for use in the block
-;;          (vars (second processed-params))
-;;          (result-params (third processed-params))
-;;          ;; either OUTPUT or VALUE which should behave as described above
-;;          (result-type (fourth processed-params))
-;;          ;; expand the body with `org-babel-expand-body:vala'
-;;          (full-body (org-babel-expand-body:vala
-;;                      body params processed-params)))
-;;     ;; actually execute the source-code block either in a session or
-;;     ;; possibly by dropping it to a temporary file and evaluating the
-;;     ;; file.
-;;     ;;
-;;     ;; for session based evaluation the functions defined in
-;;     ;; `org-babel-comint' will probably be helpful.
-;;     ;;
-;;     ;; for external evaluation the functions defined in
-;;     ;; `org-babel-eval' will probably be helpful.
-;;     ;;
-;;     ;; when forming a shell command, or a fragment of code in some
-;;     ;; other language, please preprocess any file names involved with
-;;     ;; the function `org-babel-process-file-name'. (See the way that
-;;     ;; function is used in the language files)
-;;     ))
 
-;; This function should be used to assign any variables in params in
-;; the context of the session environment.
 (defun org-babel-prep-session:vala (session params)
   "This function does nothing as Vala is a compiled language with no
 support for sessions"
